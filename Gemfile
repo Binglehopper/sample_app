71e530fc566da4669fb7463c8d46875f40952e2f
Gregory Lou <gregory.lou@gmail.com>
2011-12-18 17:12:12 -0500

    Created a user model

--- Gemfile
+++ Gemfile
@@ -8,7 +8,8 @@ gem 'rails', '3.0.1'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 
 group :development do
-   gem 'rspec-rails', '2.0.0.beta.18'
+   gem 'rspec-rails', '2.0.0.beta.18'	
+   gem 'annotate', '2.4.0'
 end
 
 group :test do
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email #specifies which attributes of the model are accessible by outside world
+end
