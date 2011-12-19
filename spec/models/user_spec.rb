4f89759349376da9930502a98af54db0ea95a2ef
Gregory Lou <gregory.lou@gmail.com>
2011-12-18 21:06:34 -0500

    Added password and encrypted password attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 require 'spec_helper'
@@ -14,3 +15,4 @@ require 'spec_helper'
 describe User do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
