3645b63c8ba333a8245bddd665ffa63c005c93fa
Gregory Lou <gregory.lou@gmail.com>
2011-12-26 16:00:07 -0500

    Friendly forwarding and correct user requirement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       render 'new'
     else # Successful signin
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
   end
   
