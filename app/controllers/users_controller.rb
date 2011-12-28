7db47d2fec1c0a439043b9c1af4e873df2c387ac
Gregory Lou <gregory.lou@gmail.com>
2011-12-27 19:39:49 -0500

    Can now destroy users (as admins)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
 
   def index
     @users = User.paginate(:page => params[:page])
@@ -34,13 +35,11 @@ class UsersController < ApplicationController
 
   #For the edit page - EDIT action
   def edit 
-    @user = User.find(params[:id])
     @title = "Edit user"
   end
   
   #For the updating of profiles - UPDATE action
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       # it worked
       redirect_to @user, :flash => {:success => "Profile updated."}
@@ -48,6 +47,12 @@ class UsersController < ApplicationController
       @title = "Edit user"
       render 'edit'
     end
+  end
+  
+  #For killing users - DESTROY action
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path, :flash => { :success => "User destroyed." }
   end
   
   private
@@ -59,5 +64,10 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end 
+    
+    def admin_user
+      user = User.find(params[:id])
+      redirect_to(root_path) if !current_user.admin? || current.user?(@user)
+    end
   
 end
